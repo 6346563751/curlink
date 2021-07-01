0eda95e898b4f16e71fe2b4a693c112c07f3e4e4
6346563751 <b0bb1e@protonmail.com>
2021-07-01 23:32:28 +0200

    :fix: add padding-top

--- components/Footbar.jsx
+++ components/Footbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const Footbar = () => {
   return (
     <>
-      <footer className="items-center justify-center w-full bg-gray-800 text-white mt-3">
+      <footer className="items-center justify-center w-full bg-gray-800 text-white mt-3 pt-4">
         <div class="container mx-auto px-6">
           <div class="sm:flex sm:mt-8">
             <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
