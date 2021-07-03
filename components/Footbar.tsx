251dd6355afe7677b5b6176e25466ff3fb8bc7eb
6346563751 <b0bb1e@protonmail.com>
2021-07-03 09:46:23 +0200

    :bug: fix build error

--- components/Footbar.tsx
+++ components/Footbar.tsx
@@ -12,24 +12,24 @@ export const Footbar = (): JSX.Element => {
               </div>
               <div className="flex flex-1 flex-col">
                 <span className="font-bold uppercase mb-2">A propos</span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Carrière</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">CGU</Link></span>
-                <span className="my-2"><Link href="/privacy" className="text-gray-300  text-md hover:text-gray-500">Confidentialité</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Contact</Link></span>
+                <span className="my-2 text-gray-300  text-md hover:text-gray-500"><Link href="#">Carrière</Link></span>
+                <span className="my-2 text-gray-300  text-md hover:text-gray-500"><Link href="#">CGU</Link></span>
+                <span className="my-2 text-gray-300  text-md hover:text-gray-500"><Link href="/privacy">Confidentialité</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Contact</Link></span>
               </div>
               <div className="flex flex-1 flex-col">
                 <span className="font-bold uppercase mt-4 md:mt-0 mb-2">Produit</span>
-                <span className="my-2"><Link href="#" className="text-gray-300 text-md hover:text-gray-500">Aide</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Association</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300 text-md hover:text-gray-500">Entreprise</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300 text-md hover:text-gray-500">Particulier</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Aide</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Association</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Entreprise</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Particulier</Link></span>
               </div>
               <div className="flex flex-1 flex-col">
                 <span className="font-bold uppercase mt-4 md:mt-0 mb-2">Social</span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Blog</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Discord</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Instagram</Link></span>
-                <span className="my-2"><Link href="#" className="text-gray-300  text-md hover:text-gray-500">Twitter</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Blog</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Discord</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Instagram</Link></span>
+                <span className="my-2 text-gray-300 text-md hover:text-gray-500"><Link href="#">Twitter</Link></span>
               </div>
             </div>
           </div>
